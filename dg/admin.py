dd6b4e1f39ad49bab1afabe014be9bf4d842ca67
nusherjk <nusherjk@gmail.com>
2021-01-12 21:56:44 +0600

    models update

--- dg/admin.py
+++ dg/admin.py
@@ -4,3 +4,7 @@ from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Product)
+admin.site.register(ProductReview)
+admin.site.register(Order)
+admin.site.register(Category)
--- dg/models.py
+++ dg/models.py
@@ -1,8 +1,48 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from uuid import uuid4
 # Create your models here.
 
 
 class User(AbstractUser):
     address = models.CharField(max_length=2000)
     contactNumber = models.CharField(max_length=15)
+
+
+# Products models
+class Category(models.Model):
+    id = models.UUIDField(auto_created=True, default=uuid4, primary_key=True, unique=True)
+    name = models.CharField(max_length=300)
+
+class Product(models.Model):
+    id = models.UUIDField(auto_created=True, default=uuid4, primary_key=True, unique=True)
+    name = models.CharField(max_length=300)
+    price = models.FloatField()
+    images = models.ImageField()
+    #brand
+    description = models.CharField(max_length=2500)
+    size = models.CharField(max_length=20) # Probable JSON field
+    #colors
+    availability = models.BooleanField()
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    details = models.CharField(max_length=2500)
+
+
+class ProductReview(models.Model):
+    id = models.UUIDField(auto_created=True, default=uuid4, primary_key=True, unique=True)
+    name = models.CharField(max_length=300)
+    email = models.EmailField()
+    review = models.CharField(max_length=3000)
+    productid = models.ForeignKey(Product, on_delete=models.CASCADE)
+    userid = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Order(models.Model):
+    id = models.UUIDField(auto_created=True, default=uuid4, primary_key=True, unique=True)
+    #name = models.CharField(max_length=300)
+    #email = models.EmailField()
+    #address = models.CharField(max_length=2000)
+    total = models.FloatField(null=True)
+    productlist = models.ManyToManyField(Product)
+    userid = models.ForeignKey(User, on_delete=models.CASCADE)
+
