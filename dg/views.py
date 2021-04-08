29b717d9792f7ea418573c69966227778e14bab1
Nusher Jamil Kazi <nusherjk@gmail.com>
2021-04-08 20:35:39 +0600

    tokenauth and create user endpoint established

--- dg/views.py
+++ dg/views.py
@@ -4,6 +4,7 @@ from .serializers import UserSerializer, ProductSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.status import HTTP_200_OK
 from rest_framework.permissions import IsAuthenticated, AllowAny
 # Create your views here.
 
@@ -12,10 +13,14 @@ class UserViwset(ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
-    @action(detail=True, methods=['post'])
+    @action(detail=True, methods=['put'])
     def perform_create(self, request):
-        #self.serializer_class(data=request.data)
-        print(request.data)
+        data =self.serializer_class(data=request.data)
+        if(data.is_valid()):
+            data.save()
+            return Response({"status": HTTP_200_OK, "data": data})
+        else:
+            return Response("Form Data is not valid")
 
 
 
@@ -24,6 +29,14 @@ class ProductViewset(ModelViewSet):
     permission_classes = [AllowAny]
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+
+    @action(detail=True, methods=['post'])
+    def perform_create(self, request):
+        data = request.data
+        serializer = self.serializer_class(data=data)
+        if(serializer.is_valid()):
+            serializer.save()
+            return Response({"status": HTTP_200_OK, "data": data})
 
 
 
--- doog/settings.py
+++ doog/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'dg',
 ]
 
@@ -136,5 +137,11 @@ STATICFILES_DIRS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+                            'rest_framework.authentication.BasicAuthentication',
+                            'rest_framework.authentication.SessionAuthentication',
+                            'rest_framework.authentication.TokenAuthentication',
+                                        ]
 }
